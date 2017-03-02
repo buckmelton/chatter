eb0172fe1f70cacfebfcf45057e8685637d12341
Buck Melton <buck9063@gmail.com>
2017-03-01 17:23:37 -0800

    Remove test for deleted Contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Chatter"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Chatter"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Chatter"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Chatter"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
