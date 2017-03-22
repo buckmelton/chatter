03b43fb0267ffd4a31e1c60c28e9ea40a5c547a4
Buck Melton <buck9063@gmail.com>
2017-03-22 12:36:33 -0700

    Add name presence validation and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,18 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = User.new(name: "Buck", email: "buck@buck.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+  
 end
