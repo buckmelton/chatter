d93c347ba146061c328fe05d035b857a65e9215f
Buck Melton <buck9063@gmail.com>
2017-03-30 14:29:45 -0700

    Stub user#create and set required/permitted params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful save
+    else
+      render 'new'  # Go back to 'new' page
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
